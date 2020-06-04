73258cdb3b90916ba9d93400e60a0ee29fdf730b
juliakubiczek <66443422+juliakubiczek@users.noreply.github.com>
2020-06-04 22:55:19 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -4,6 +4,7 @@ echo ("<br> Jestem programem 01");
 echo ("<br> Zostałem stworzony na zajęcia z Informatyki");
 echo ("<br> To nie było trudne");
 echo ("<br> Dalsza czesc programu");
+echo ("<br> Kolejna czesc programu");
 
 ?>
 
