c3a4d70a798554943c1d72c2881e9cb647f58ba3
JuliaK <julia.kubiczek99@gmail.com>
2020-06-04 22:41:37 +0200

    Czwarty commit

--- 01program.php
+++ 01program.php
@@ -1,7 +1,10 @@
 <?php
 echo ("Witaj");
 echo ("<br> Jestem programem 01");
-echo ("<br> Test");
+echo ("<br> Zostałem stworzony na zajęcia z Informatyki");
+echo ("<br> To nie było trudne");
+echo ("<br> Dalsza czesc programu");
+
 ?>
 
 
